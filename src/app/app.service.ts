19004dad70667585c9ca44d97d927be8ed890785
Kasianov Alexander <torrrka@gmail.com>
2020-07-20 15:03:42 +0300

    tt

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -4,8 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppService {
-  test: string;
+  test = 'test';
   gitCommit = '--amend';
-
   constructor() { }
 }
